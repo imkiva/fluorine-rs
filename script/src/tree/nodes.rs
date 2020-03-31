115a745660e479ca4e0b0b5da13bd7103caebd4c
imkiva <libkernelpanic@gmail.com>
2020-03-31 23:18:33 +0800

    parser: ast nodes

---
+++ script/src/tree/nodes.rs
@@ -0,0 +1,29 @@
+type Name = String;
+
+#[derive(Debug)]
+pub enum Lit {
+    Number(f64),
+    String(String),
+}
+
+#[derive(Debug)]
+pub enum Atom {
+    Literal(Lit),
+    Id(Name),
+    Lambda(i32, Vec<Expr>),
+}
+
+#[derive(Debug)]
+pub enum Expr {
+    Atom(Atom),
+    DBI(i32),
+    Unary(char, Box<Expr>),
+    Binary(char, Box<Expr>, Box<Expr>),
+    Apply(Box<Expr>, Vec<Expr>),
+}
+
+#[derive(Debug)]
+pub enum Decl {
+    Let(Name, Expr),
+}
+
