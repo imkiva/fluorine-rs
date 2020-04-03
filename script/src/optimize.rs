00bb6383b09aa0d8e25281a35b9d8364cb963632
imkiva <libkernelpanic@gmail.com>
2020-04-04 01:15:25 +0800

    optimize: aggressive: contextual partial eval

--- script/src/optimize.rs
+++ script/src/optimize.rs
@@ -1,5 +1,9 @@
-use crate::tree::Program;
-use crate::pe::PartialEval;
+use crate::tree::{Program, Expr};
+use crate::pe::{PartialEval, PEContext};
+use crate::tree::ProgramItem::DeclItem;
+use crate::tree::Decl::LetDecl;
+
+use std::collections::HashMap;
 
 pub struct Optimizer;
 
@@ -14,7 +18,65 @@ impl Optimizer {
         match level {
             OptimizeLevel::NONE => input,
             OptimizeLevel::NORMAL => input.partial_eval(),
-            OptimizeLevel::AGGRESSIVE => input.partial_eval(),
+            OptimizeLevel::AGGRESSIVE => {
+                let mut ctx = OptimizeContext::new();
+                ctx.prepare(&input);
+                input.partial_eval_with(Some(&ctx))
+            }
+        }
+    }
+}
+
+struct OptimizeContext {
+    vars: HashMap<String, Expr>,
+}
+
+impl OptimizeContext {
+    fn new() -> OptimizeContext {
+        OptimizeContext {
+            vars: Default::default(),
+        }
+    }
+
+    fn prepare(&mut self, program: &Program) {
+        program.into_iter().for_each(|item| match item {
+            DeclItem(LetDecl(name, expr)) => {
+                self.vars.insert(name.clone(), expr.clone());
+            }
+            _ => (),
+        });
+    }
+}
+
+impl PEContext for OptimizeContext {
+    fn try_resolve_constant(&self, name: &str) -> Option<Expr> {
+        self.vars.get(name).map(|e| e.clone())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parse::FsParser;
+    use crate::optimize::Optimizer;
+    use crate::optimize::OptimizeLevel::AGGRESSIVE;
+
+    #[test]
+    fn test_aggressive() {
+        let ast = FsParser::ast("\
+            let a = 1 + 1 + 3\n
+            let b = 4 + 5 + 6\n
+            let times = { a, b -> a * b }\n
+            let result = times(a, b)\n
+            let const = { a, b -> a }\n
+            let id = { a -> a }\n
+            let const_id = const(id)\n
+            \
+        ").map(|ast| Optimizer::run(ast, AGGRESSIVE));
+
+        if let Ok(p) = ast {
+            println!("{:#?}", p);
+        } else {
+            eprintln!("Internal error")
         }
     }
 }
