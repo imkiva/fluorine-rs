d84be695f2a7e75c8c78eac9b595566645c9647f
imkiva <libkernelpanic@gmail.com>
2020-04-03 15:18:32 +0800

    subst: subst trait for private use

--- script/src/subst.rs
+++ script/src/subst.rs
@@ -4,31 +4,61 @@ use crate::tree::Atom::AtomLambda;
 
 pub struct Substitution;
 
-impl Substitution {
-    pub fn subst(dbi: i32, expr: Expr, replacement: &Expr) -> Expr {
-        match &expr {
+trait Subst {
+    type Output;
+    fn subst(self: Self, dbi: i32, replacement: &Expr) -> Self::Output;
+}
+
+impl<T: Subst<Output=T>> Subst for Box<T> {
+    type Output = Box<T>;
+
+    fn subst(self: Self, dbi: i32, replacement: &Expr) -> Self::Output {
+        let t = *self;
+        Box::new(t.subst(dbi, replacement))
+    }
+}
+
+impl<T: Subst<Output=T>> Subst for Vec<T> {
+    type Output = Vec<T>;
+
+    fn subst(self: Self, dbi: i32, replacement: &Expr) -> Self::Output {
+        self.into_iter()
+            .map(|expr| expr.subst(dbi, replacement))
+            .collect()
+    }
+}
+
+impl Subst for Expr {
+    type Output = Expr;
+
+    fn subst(self: Self, dbi: i32, replacement: &Expr) -> Self::Output {
+        match &self {
             DBI(i) if dbi == *i => replacement.clone(),
-            DBI(_) => expr,
+            DBI(_) => self,
 
             UnaryExpr(op, unary) =>
-                UnaryExpr(op.clone(), Box::new(Substitution::subst(dbi, *unary.clone(), replacement))),
+                UnaryExpr(op.clone(), unary.clone().subst(dbi, replacement)),
 
             BinaryExpr(op, lhs, rhs) =>
-                BinaryExpr(op.clone(), Box::new(Substitution::subst(dbi, *lhs.clone(), replacement)),
-                           Box::new(Substitution::subst(dbi, *rhs.clone(), replacement))),
+                BinaryExpr(op.clone(), lhs.clone().subst(dbi, replacement),
+                           rhs.clone().subst(dbi, replacement)),
 
             AtomExpr(AtomLambda(nested_argc, nested_dbi, nested_body)) => {
-                let new_body: Vec<Expr> = nested_body.into_iter()
-                    .map(|expr| Substitution::subst(nested_argc + dbi, expr.clone(), replacement))
-                    .collect();
-                AtomExpr(AtomLambda(*nested_argc, *nested_dbi, new_body))
+                AtomExpr(AtomLambda(*nested_argc, *nested_dbi,
+                                    nested_body.clone().subst(nested_argc + dbi, replacement)))
             }
 
             ApplyExpr(f, arg) =>
-                ApplyExpr(Box::new(Substitution::subst(dbi, *f.clone(), replacement)),
-                          Box::new(Substitution::subst(dbi, *arg.clone(), replacement))),
+                ApplyExpr(f.clone().subst(dbi, replacement),
+                          arg.clone().subst(dbi, replacement)),
 
-            _ => expr,
+            _ => self,
         }
+    }
+}
+
+impl Substitution {
+    pub fn subst(dbi: i32, expr: Expr, replacement: &Expr) -> Expr {
+        expr.subst(dbi, replacement)
     }
 }
