b39b6b1022053776f2d2ae6d2d38efb004a3f365
imkiva <libkernelpanic@gmail.com>
2020-04-06 22:31:03 +0800

    main: command line parser

--- src/gui/gui_main.rs
+++ src/gui/gui_main.rs
@@ -1,3 +1,2 @@
 pub(crate) fn gui_main() {
-
 }
--- src/mod.rs
+++ src/mod.rs
@@ -1,7 +1,19 @@
 pub mod cli;
 pub mod gui;
 
+use clap::App;
+
 fn main() {
-    cli::cli_main::cli_main();
-    gui::gui_main::gui_main();
+    let args = App::new("flourine")
+        .version("0.1.0")
+        .author("Kiva <imkiva@icloud.com>")
+        .about("Fluorine interactive math")
+        .args_from_usage(
+            "-i, --repl        'Starts an interactive fluorine shell'")
+        .get_matches();
+
+    match args.is_present("repl") {
+        true => cli::cli_main::cli_main(),
+        _ => gui::gui_main::gui_main(),
+    }
 }
