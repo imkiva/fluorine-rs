9bb38d164e291a9a7110e26d17e6c2c9e10ea098
imkiva <imkiva@icloud.com>
2020-07-06 14:29:19 +0800

    cfl: value

--- c-runtime/value.h
+++ c-runtime/value.h
@@ -3,4 +3,27 @@
 //
 #pragma once
 
+#include <stdbool.h>
+#include "string.h"
 
+typedef double f64;
+
+typedef enum {
+    UnitType,
+    NumberType,
+    BoolType,
+    StringType,
+    LambdaType,
+    EnumType,
+} FLTypeTag;
+
+typedef struct {
+    FLTypeTag tag;
+    union {
+        f64 number;
+        bool boolean;
+        String *string;
+        void *lambda;
+        void *enu;
+    };
+} FLValue;
